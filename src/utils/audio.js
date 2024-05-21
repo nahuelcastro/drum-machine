1df43f955bdfdb174652ece646fb1e51168ba284
nahuelcastro <29667716+nahuelcastro@users.noreply.github.com>
2024-05-21 23:14:32 +0200

    buttons and audio when click ok

---
+++ src/utils/audio.js
@@ -0,0 +1,13 @@
+const audioSourceMap = {
+    "Q": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3",
+    "W": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3",
+    "E": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3",
+    "A": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3",
+    "S": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3",
+    "D": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3",
+    "Z": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3",
+    "X": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
+    "C": "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3",
+}
+
+export default audioSourceMap;
